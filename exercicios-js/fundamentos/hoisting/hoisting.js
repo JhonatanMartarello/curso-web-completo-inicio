3ff74462d5e65e22f388ef43ca709139ee6e2455
JhonatanMartarello <jhonatan.martarello@gmail.com>
2021-08-05 20:52:13 -0300

    hoisting js

---
+++ exercicios-js/fundamentos/hoisting/hoisting.js
@@ -0,0 +1,11 @@
+/* hoisting é quando ocorre o içamento de uma variável. 
+ * No exemplo abaixo, "a" foi definida depois do console.log, porém não ocorre ERRO.
+*/
+
+console.log("a =", a); // undefined
+var a = 2;
+console.log("a = ", a); // 2
+
+// console.log("b = ", b); // ERROR. Não ocorre içamento com LET.
+let b = 3;
+console.log("b = ", b); // 3
